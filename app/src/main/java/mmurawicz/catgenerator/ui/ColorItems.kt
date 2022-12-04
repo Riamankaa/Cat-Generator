8ad0ec9d0c4e481ff797b73dca00a84656a7e9ac
Riamankaa <murawicz.marianna@gmail.com>
2022-12-04 18:47:57 +0100

    Change the colors from list to catty colors

--- app/src/main/java/mmurawicz/catgenerator/ui/ColorItems.kt
+++ app/src/main/java/mmurawicz/catgenerator/ui/ColorItems.kt
@@ -5,24 +5,28 @@ import mmurawicz.catgenerator.R
 object ColorItems {
     val list: List<ColorItem> = listOf(
         ColorItem(
-            text = R.string.color_black,
-            color = R.color.black
+            text = R.string.color_violet_catnip,
+            color = R.color.violet_catnip
         ),
         ColorItem(
-            text = R.string.color_white,
-            color = R.color.white
+            text = R.string.color_orange_salmon,
+            color = R.color.orange_salmon
         ),
         ColorItem(
-            text = R.string.color_blue,
-            color = R.color.blue
+            text = R.string.color_grey_mousey,
+            color = R.color.grey_mousey
         ),
         ColorItem(
-            text = R.string.color_red,
-            color = R.color.red
+            text = R.string.color_red_laser,
+            color = R.color.red_laser
         ),
         ColorItem(
-            text = R.string.color_yellow,
-            color = R.color.yellow
+            text = R.string.color_brown_cartoon,
+            color = R.color.brown_cartoon
+        ),
+        ColorItem(
+            text = R.string.color_white_milky,
+            color = R.color.white_milky
         )
     )
 }
